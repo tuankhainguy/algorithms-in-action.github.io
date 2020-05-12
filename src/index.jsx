bba02041c39597e29799dc482a02ced235403e11
BOHAO <bohao.liu@student.unimelb.edu.au>
2020-05-13 06:57:05 +0800

    Added the LeftPanel

--- src/index.jsx
+++ src/index.jsx
@@ -1,13 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
+import ClippedDrawer from './LeftPanel';
 import * as serviceWorker from './serviceWorker';
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <ClippedDrawer />
   </React.StrictMode>,
+
   document.getElementById('root'),
 );
 
