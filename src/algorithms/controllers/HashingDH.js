a73f6d8f38f6bd59ce92ae42f2ee672bd1511255
Khai Nguy <khaint0204@gmail.com>
2024-09-03 15:26:16 +1000

    test

--- src/algorithms/controllers/HashingDH.js
+++ src/algorithms/controllers/HashingDH.js
@@ -17,7 +17,7 @@ export default {
         vis.array.set(array, 'HashingDH');
         vis.array.hideArrayAtIndex(2);
       },
-      [[1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11], [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0]]
+      [[[1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11], [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0]]]
     );
   },
 };
